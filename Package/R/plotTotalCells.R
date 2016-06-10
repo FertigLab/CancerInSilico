2a39ba839cb0abef9df2045d9e1c9017b8f7e6de
Tom Sherman <tsherma4@jhu.edu>
2016-06-10 12:13:57 -0400

    added methods for the s4 object returned

---
+++ Package/R/plotTotalCells.R
@@ -0,0 +1,18 @@
+setGeneric("plotTotalCells", function(mat)
+            standardGeneric("plotTotalCells"))
+
+setMethod("plotTotalCells", "cellMatrix",
+
+  function(mat) {
+  
+    total_cells = c()
+    radii = seq(3,ncol(mat),6)
+  
+    for (t in 1:nrow(mat)) {
+      total_cells[t] = sum(mat[t,radii]>0)
+    }
+  
+    plot(total_cells)
+  }
+
+)
