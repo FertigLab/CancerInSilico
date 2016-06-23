3dc48461009fb2f74fa6cda412d146af6f65ba3a
Raymond Cheng <rchighlander296@gmail.com>
2016-06-23 09:18:32 -0400

    Delete Install.R

--- Install.R
+++
@@ -1,8 +0,0 @@
-#path = "E:/Repository/CellBasedModel"
-
-#library(Rcpp)
-#library(testthat)
-# compileAttributes(paste(path,"/Package/",sep=""))
-# install.packages(paste(path,"/Package/",sep=""),repos=NULL,type="binary")
-Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
-install.packages("CellModel_1.0.tar.gz",repos=NULL)
