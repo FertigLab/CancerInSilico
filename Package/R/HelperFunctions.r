467c35f51eb5b8b873dcb902cae5616bbf1b6c5e
Tom Sherman <tsherma4@jhu.edu>
2016-06-24 10:41:26 -0400

    changed names, updated version number

--- Package/R/HelperFunctions.r
+++ Package/R/HelperFunctions.r
@@ -1,7 +1,4 @@
-
-
-#Add Circle creates cells
-  AddCircle <- function(x,y,rad) {
-    theta <- seq(0,2*pi,length=200)
-    lines(x + rad * cos(theta),y + rad * sin(theta))
-  }
+DrawCircle <- function(x,y,rad) {
+  theta <- seq(0,2*pi,length=200)
+  lines(x + rad * cos(theta),y + rad * sin(theta))
+}
