d3ed951bcb9d24b5d995334aabaeed857474ad86
Tom Sherman <tsherma4@jhu.edu>
2016-06-28 11:27:35 -0400

    added vignette

--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -1,6 +1,6 @@
 DrawCircle <- function(x,y,rad) {
 
     theta <- seq(0,2*pi,length=200)
-    plot(x + rad * cos(theta),y + rad * sin(theta),type="l",new=F)
+    plot(x + rad * cos(theta),y + rad * sin(theta),type="l",new=FALSE)
 
 }
