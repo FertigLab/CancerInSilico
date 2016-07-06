5e5e14d871f13edccc26cb85cfd47de064f5a1c1
Raymond Cheng <rchighlander296@gmail.com>
2016-07-06 19:05:22 -0400

    Completed Interactivity Plot, Finishing Vignette

--- R/plotCellsAtTime.R
+++ R/plotCellsAtTime.R
@@ -25,78 +25,36 @@ setMethod("plotCellsAtTime", "CellMatrix",
         mn = min(min(mat[,xcoords]),min(mat[,ycoords])) - 2
         mx = max(max(mat[,xcoords]),max(mat[,ycoords])) + 2
 
+        #Opens new device to put plot into if no plot exists already
         
-        #Opens new device to put plot into
-        dev.new()
-        #dev.next(which = dev.cur())
-        dev.set(which = 1)
-
+        if(identical(dev.list(),NULL) == TRUE){
+          dev.new()
+          dev.set(which = 1)
+        }
+        dev.set(which = 4)
         
         testcase = "abcdefghij"
         theta = seq(0,2*pi,length=200)
-        read = " "
         #Currently Assuming All Cells are Alive (No Cell Death)
-        while(time <= nrow(data)) {
-          if(identical(read,"q")){
-            dev.off()
-            break
-          }
-          else if(identical(read,"summ")){
-            print(paste("Number of Cells at time",time,"=",test))
-          }
-          else if(suppressWarnings(identical(as.numeric(read),as.numeric(testcase)) == FALSE)){
-            time = as.numeric(read)
-          }
-          else if(identical(substring(read,1,1),"b")){
-            temp = 1
-            if(identical(substring(gsub(" ","",read),2,3),"-j")){
-              temp = as.numeric(substring(gsub(" ","",read),4))
-              time = time - temp
-            }
-            else{
-              print(time)
-              time = time - temp
-            }
-          }
-          else if(identical(substring(read,1,1),"n")){
-            temp = 1
-            if(identical(substring(gsub(" ","",read),2,3),"-j")){
-              temp = as.numeric(substring(gsub(" ","",read),4))
-              time = time + temp
-            }
-            else{
-              print(time)
-              time = time + temp
-            }
-          }
-          else{
-            print("Enter a valid command.")
-          }
-          if(time > nrow(data) | time < 0){
-            print("Time out of bound")
-            dev.off()
-            break
-          }
-          plot(c(mn,mx),c(mn,mx),main=paste("Plot of CellModel At Time",time),type="n")
-          test = sum(mat[time,radii]>0)  
-          for (n in xcoords) {
-    
-              x_1 =  mat[time,n] + (- 0.5 * mat[time,n+3] + mat[time,n+2]) *
-                      cos(mat[time,n+4])
-              y_1 =  mat[time,n+1] + (- 0.5 * mat[time,n+3] + mat[time,n+2]) *
-                      sin(mat[time,n+4])
-              x_2 =  mat[time,n] + (0.5 * mat[time,n+3] - mat[time,n+2]) *
-                      cos(mat[time,n+4])
-              y_2 =  mat[time,n+1] + (0.5 * mat[time,n+3] - mat[time,n+2]) *
-                      sin(mat[time,n+4])
-              
-              lines(x_1 + mat[time,n+2] * cos(theta),y_1 + mat[time,n+2] * sin(theta),type="l",new=FALSE)
-              lines(x_2 + mat[time,n+2] * cos(theta),y_2 + mat[time,n+2] * sin(theta),type="l",new=FALSE)
-              # DrawCircle(x_1,y_1,mat[time,n+2])
-              # DrawCircle(x_2,y_2,mat[time,n+2])
-          }
-            read = readline()
+        plot(c(mn,mx),c(mn,mx),main=paste("Plot of CellModel At Time",time),type="n")
+          
+        for (n in xcoords) {
+  
+            x_1 =  mat[time,n] + (- 0.5 * mat[time,n+3] + mat[time,n+2]) *
+                    cos(mat[time,n+4])
+            y_1 =  mat[time,n+1] + (- 0.5 * mat[time,n+3] + mat[time,n+2]) *
+                    sin(mat[time,n+4])
+            x_2 =  mat[time,n] + (0.5 * mat[time,n+3] - mat[time,n+2]) *
+                    cos(mat[time,n+4])
+            y_2 =  mat[time,n+1] + (0.5 * mat[time,n+3] - mat[time,n+2]) *
+                    sin(mat[time,n+4])
+            
+            lines(x_1 + mat[time,n+2] * cos(theta),y_1 + mat[time,n+2] * sin(theta),type="l",new=FALSE)
+            lines(x_2 + mat[time,n+2] * cos(theta),y_2 + mat[time,n+2] * sin(theta),type="l",new=FALSE)
+            # DrawCircle(x_1,y_1,mat[time,n+2])
+            # DrawCircle(x_2,y_2,mat[time,n+2])
         }
+        
       }
 
 )
---
+++ R/plotInteractive.R
@@ -0,0 +1,76 @@
+#' \code{plotInteractive} Plots a CellModel at a certain point in time
+#'
+#' @param mat A Cell Model object
+#' @param time The timestep at which to plot the matrix. Must be below
+#'      the specified max amount of timesteps
+#' @return Plot a visual representation of cells that 
+#' takes in command-line-like inputs as well
+#' @export
+
+setGeneric("plotInteractive", function(mat,time)
+  standardGeneric("plotInteractive"))
+
+setMethod("plotInteractive", "CellMatrix",
+          
+          function(mat,time){
+            while(time <= nrow(data)) {
+              radii = seq(3,ncol(mat),6)
+              grrate = seq(6,ncol(mat),6)
+              plotCellsAtTime(mat,time)
+              read = readline()
+              count = 1
+              place = unlist(gregexpr(" ",read))[1]
+              if(place == -1){
+                place = nchar(read)
+              }
+              cmd = gsub(" ","",substring(read,1,place))
+              argunum = suppressWarnings(as.numeric(gsub(" ","",substring(read,place,nchar(read)))))
+              arguchr = gsub(" ","",substring(read,place,nchar(read)))
+              testcase = suppressWarnings(as.numeric("abcdefghijklmnopqrstuvwxyz"))
+              cmds <- c("n","b","t","summ","q","h")
+              if((cmd %in% cmds)==TRUE){
+                if(suppressWarnings(identical(argunum,testcase) == FALSE)){
+                  count = argunum
+                }
+                if(match(cmd,cmds) == 1){
+                  time = time + count
+                }
+                else if(match(cmd,cmds) == 2){
+                  time = time - count
+                }
+                else if(match(cmd,cmds) == 3){
+                  time = count
+                }
+                else if(match(cmd,cmds) == 4){
+                  vari = sum(mat[time,radii]>0)
+                  if(arguchr == "d"){
+                    vari = getDensity(mat,time)
+                    cat(paste("Density of Cells at time",time,"=",vari))
+                  }
+                  else if(arguchr == "g"){
+                    vari = (sum(mat[time,grrate]))/(sum(mat[time,radii]))
+                    cat(paste("Average Growth Rate of Cells at time",time,"=",vari))
+                  }
+                  else{
+                    cat(paste("Number of Cells at time",time,"=",vari))
+                  }
+                }
+                else if(match(cmd,cmds) == 5){
+                  graphics.off()
+                  break
+                }
+                else if(match(cmd,cmds) == 6){
+                  cat("Basic Commands: \n b = back one timestep \n n = forward one timestep \n summ = summary of cells \n q = quit \"console\"\n h = basic command help")
+                }
+              }
+              else{
+                cat("Enter a valid command. Type \"h\" for further help")
+              }
+              if(time > nrow(data) | time <= 0){
+                print("Time out of bound")
+                graphics.off()
+                break
+              }
+            }
+          }
+)
