568adef696f9342978823eec60e30360c8855540
Tom Sherman <tsherma4@jhu.edu>
2016-07-22 13:29:54 -0400

    added timeToRow - converts model time to row in output list

---
+++ R/timeToRow.R
@@ -0,0 +1,12 @@
+setGeneric("timeToRow", function(model, time)
+  standardGeneric("timeToRow"))
+
+setMethod("timeToRow", "CellModel",
+          
+	function(model, time) {
+
+        return(floor(time / model@parameters[6]) + 1)
+
+	}
+         
+)
