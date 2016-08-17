918c91b6f079d1cd912291f65c7b523cc613aeae
ejfertig <ejfertig@jhmi.edu>
2016-08-17 18:52:33 -0400

    changed indentation in NBsim.R

--- R/NBsim.R
+++ R/NBsim.R
@@ -1,5 +1,5 @@
 setGeneric("NBsim", function(mu, ReferenceDataset=NULL, invChisq=TRUE, ...)
-    standardGeneric("NBsim"))
+  standardGeneric("NBsim"))
 
 ### Function to simulate negative binomial distribution from count data
 NBsim<- function(mu, # mean expression values for each gene
@@ -9,41 +9,41 @@ NBsim<- function(mu, # mean expression values for each gene
   
   
   
-    if(is.null(ReferenceDataset)){
-        ngenes<-length(mu)
-        mu.Reference <- pmax(mu,1)
-    }
-    else {
-        
-        # check that the reference dataset is a valid log transformed dataset
-        # with values for every gene in mu
-        checkReferenceDataset(ReferenceDataset, pathways = list(row.names(mu)))
-        
-        
-        #number of genes  
-        ngenes<-dim(ReferenceDataset)[1]
-        
-        # assume that the reference dataset is log transformed so 
-        # convert to counts
-        mu.Reference<-pmax(round(rowMeans(2^ReferenceDataset - 1)),1)
-        }
-        
-        # Biological variation
-        BCV0 <- 0.2+1/sqrt(mu.Reference)
+  if(is.null(ReferenceDataset)){
+    ngenes<-length(mu)
+    mu.Reference <- pmax(mu,1)
+  }
+  else {
     
-        if(invChisq){
-            df.BCV <- 40
-            BCV <- BCV0*sqrt(df.BCV/rchisq(ngenes,df=df.BCV))
+    # check that the reference dataset is a valid log transformed dataset
+    # with values for every gene in mu
+    checkReferenceDataset(ReferenceDataset, pathways = list(row.names(mu)))
     
-        } 
-        else {
-            BCV <- BCV0*exp(rnorm(ngenes,mean=0,sd=0.25)/2 )
-        }
-        #if(NCOL(BCV)==1) BCV <- matrix(BCV,ngenes)
-        shape <- 1/BCV^2
-        scale <- mu/shape
-        mu <- matrix(rgamma(ngenes,shape=shape,scale=scale),ngenes)
-        # Technical variation
-        counts <- matrix(rpois(ngenes,lambda=mu),ngenes)
-        return(counts)
+    
+    #number of genes  
+    ngenes<-dim(ReferenceDataset)[1]
+    
+    # assume that the reference dataset is log transformed so 
+    # convert to counts
+    mu.Reference<-pmax(round(rowMeans(2^ReferenceDataset - 1)),1)
+  }
+  
+  # Biological variation
+  BCV0 <- 0.2+1/sqrt(mu.Reference)
+  
+  if(invChisq){
+    df.BCV <- 40
+    BCV <- BCV0*sqrt(df.BCV/rchisq(ngenes,df=df.BCV))
+    
+  } 
+  else {
+    BCV <- BCV0*exp(rnorm(ngenes,mean=0,sd=0.25)/2 )
+  }
+  #if(NCOL(BCV)==1) BCV <- matrix(BCV,ngenes)
+  shape <- 1/BCV^2
+  scale <- mu/shape
+  mu <- matrix(rgamma(ngenes,shape=shape,scale=scale),ngenes)
+  # Technical variation
+  counts <- matrix(rpois(ngenes,lambda=mu),ngenes)
+  return(counts)
 }
