a13c67fe4804b519ae454383703f91f207c2ef3d
ejfertig <ejfertig@jhmi.edu>
2016-08-17 20:20:45 -0400

    moved all polyester simulations to one function

--- R/inSilicoRNASeq.R
+++ R/inSilicoRNASeq.R
@@ -37,42 +37,7 @@ inSilicoRNASeq <- function(CellModels, pathways=NULL, ReferenceDataSet=NULL, fas
     # run polyester
     message('Simulating time-course RNA-seq data with Polyester')
     
-    # process fasta to create mean values for transcripts
-    transcripts = readDNAStringSet(myfasta) # read in the fasta
-    tmpsplit=strsplit(names(transcripts),split=attrsep) # split the fasta file into elements
-    
-    tmpl=unlist(lapply(tmpsplit, function(x) x[grep(fastaGeneColumn,x)])) #grab the gene name element
-    
-    geti=function(x,i) return(x[i]) #supportive function
-    
-    #gnames may be created with something too specific to this particular one?
-    gnames=sapply(strsplit(as.character(tmpl),split=":",fixed=T),geti,i=2) #grab the second element after the colon, the gene name
-    
-    # check that using valid columns of the fasta file
-    if (!all(unique(unlist(pathways)) %in% gnames)) {
-      stop(paste(fastaGeneColumn, 'does not correspond to the annotation of genes in pathways'))
-    }
-    
-    # set the newmu to the length of the fasta, set all values to 0, 
-    # the mus are then put in, divided by the number of transcripts for each gene
-    newmu <- matrix(0, nrow=length(gnames), ncol=ncol(simMeanExprs),
-                    dimnames = list(gnames, colnames(simMeanExprs)))
-    
-    for (j in 1:ncol(simMeanExprs)) {
- 
-      
-      for(i in intersect(names(newmu),names(mu))){ #for each gene that is in common between the input and the fasta,
-        newmu[which(gnames==i),j]=simMeanExprs[i,j]/length(which(row.names(newmu)==i)) #take the provided gene data to each element with that gene of the fasta, divide by how many elements are that gene in the fasta
-        
-      }
-      
-    }
-    
-
-    # run Polyester to simulate gene expression data
-    simulate_experiment_countmat(fasta=myfasta,
-                                 num_reps=1, readmat= newmu,
-                                 outdir=outdirs,idfield=fastaGeneColumn, ...)
+    simulatePolyester(simMeanExprs=simMeanExprs,fasta=fasta, attrsep = attrsep, ...)
   }
   
 }
--- R/inSilicoSCRNASeq.R
+++ R/inSilicoSCRNASeq.R
@@ -24,7 +24,7 @@ inSilicoSCRNASeq <- function(CellModels, ncells=96, pathways=NULL, ReferenceData
   
   
   if (is.null(fasta)) {
-    # simulate data with a negative binomial error model
+    # simulate single cell data with a negative binomial error model
     message('Simulating time-course RNA-seq data with a negative binomial error model')
     
     outData <- apply(simMeanExprs,2,function(mu){NBsim(mu=mu,
@@ -36,44 +36,9 @@ inSilicoSCRNASeq <- function(CellModels, ncells=96, pathways=NULL, ReferenceData
   } else { 
     
     # run polyester
-    message('Simulating time-course RNA-seq data with Polyester')
+    message('Simulating time-course single cell RNA-seq data with Polyester')
     
-    # process fasta to create mean values for transcripts
-    transcripts = readDNAStringSet(myfasta) # read in the fasta
-    tmpsplit=strsplit(names(transcripts),split=attrsep) # split the fasta file into elements
-    
-    tmpl=unlist(lapply(tmpsplit, function(x) x[grep(fastaGeneColumn,x)])) #grab the gene name element
-    
-    geti=function(x,i) return(x[i]) #supportive function
-    
-    #gnames may be created with something too specific to this particular one?
-    gnames=sapply(strsplit(as.character(tmpl),split=":",fixed=T),geti,i=2) #grab the second element after the colon, the gene name
-    
-    # check that using valid columns of the fasta file
-    if (!all(unique(unlist(pathways)) %in% gnames)) {
-      stop(paste(fastaGeneColumn, 'does not correspond to the annotation of genes in pathways'))
-    }
-    
-    # set the newmu to the length of the fasta, set all values to 0, 
-    # the mus are then put in, divided by the number of transcripts for each gene
-    newmu <- matrix(0, nrow=length(gnames), ncol=ncol(simMeanExprs),
-                    dimnames = list(gnames, colnames(simMeanExprs)))
-    
-    for (j in 1:ncol(simMeanExprs)) {
-      
-      
-      for(i in intersect(names(newmu),names(mu))){ #for each gene that is in common between the input and the fasta,
-        newmu[which(gnames==i),j]=simMeanExprs[i,j]/length(which(row.names(newmu)==i)) #take the provided gene data to each element with that gene of the fasta, divide by how many elements are that gene in the fasta
-        
-      }
-      
-    }
-    
-    
-    # run Polyester to simulate gene expression data
-    simulate_experiment_countmat(fasta=myfasta,
-                                 num_reps=1, readmat= newmu,
-                                 outdir=outdirs,idfield=fastaGeneColumn, ...)
+    simulatePolyester(simMeanExprs=simMeanExprs,fasta=fasta, attrsep = attrsep, ...)
   }
   
 }
