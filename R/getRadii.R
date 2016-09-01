60435f94d18de5826b3729cee279fe3a1810758a
Tom Sherman <tsherma4@jhu.edu>
2016-09-01 13:16:59 -0400

    finished function to get cells transitioning between phases - untested

--- R/getRadii.R
+++ R/getRadii.R
@@ -6,7 +6,21 @@ setMethod("getRadii", "CellModel",
 	function(model, time) {
 
         indices <- seq(3,length(model@cells[[timetoRow(model,t)]]),6)
-        return(radii <- model@cells[[timetoRow(model,t)]][radii])
+        return(model@m_cells[[timetoRow(model,t)]][indices])
+
+	}
+         
+)
+
+setGeneric("getAxisLength", function(model, time)
+  standardGeneric("getAxisLength"))
+
+setMethod("getAxisLength", "CellModel",
+          
+	function(model, time) {
+
+        indices <- seq(4,length(model@cells[[timetoRow(model,t)]]),6)
+        return(model@m_cells[[timetoRow(model,t)]][indices])
 
 	}
          
--- R/getTransitionCells.R
+++ R/getTransitionCells.R
@@ -12,8 +12,28 @@ setGeneric("getTransitionCells", function(model,time,phase)
 setMethod("getTransitionCells", "CellModel",
     function(model,time,phase) {
 
-        time_window = model@parameters
+        time_window = 0.05 # arbitrary for now
 
         if (phase == 'S') {
+
+            cur_rad <- getRadii(model, t)
+            next_rad <- getRadii(model, t + time_window)
+            return (which(next_rad > sqrt(3/2) & cur_rad < sqrt(3/2)))
+
+        } else if (phase == 'M') {
+
+            cur_axis <- getAxisLength(model, t)
+            next_axis <- getAxisLength(model, t + time_window)
+            return (which(next_axis < cur_axis))
+
+        } else {
+
+            stop("getTransitionCells: invalid phase parameter")
+
+        }
+
+    }
+
+)
 
             
