cdc545002d313d0e51018834101008e2f0c398c1
Tom Sherman <tsherma4@jhu.edu>
2016-09-30 02:28:19 -0400

    moved functions on CellModel objects to CellModel-class.R, added comments to some functions

--- R/CellModel-class.R
+++ R/CellModel-class.R
@@ -1,20 +1,19 @@
-#' An S4 class to represent the output of a cell-based model
+#### class definition ####
+
+#' @title CellModel
+#' @description An S4 class to represent the output of a cell-based model
 #'
-#' @slot m_cells A list object where each row of the list describes the state
-#' of all the cells in the model at a given time. Each cell is described over
-#' 6 columns: [1] x-coordinate, [2] y-coordinate, [3] radius, [4] axis length,
-#' [5] axis angle, [6] growth rate. For instance, the x-coordinates of the first
-#' 3 cells will be in columns 1,7,13.
-#' @slot m_initialNumCells the initial number of cells in the model
-#' @slot m_runTime the total run time (hours) of the model
-#' @slot m_initialDensity the density the cells were seeded at
-#' @slot m_inheritGrowth whether or not cells inherit growth rates from their parent
-#' @slot m_outputIncrement the frequency of print statements during the run
-#' @slot m_randSeed the random seed 
-#' @slot m_epsilon model specific parameter 
-#' @slot m_nG model specific parameter
-#' @slot m_timeIncrement amount of time elapsed in each model step
-#' @slot m_cycleLengthDist initial distribution of cell-cycle lengths 
+#' @slot mCells A list object where each row of the list describes the state of all the cells in the model at a given time. Each cell is described over 6 columns: [1] x-coordinate, [2] y-coordinate, [3] radius, [4] axis length, [5] axis angle, [6] growth rate. For instance, the x-coordinates of the first 3 cells will be in columns 1,7,13.
+#' @slot mInitialNumCells the initial number of cells in the model
+#' @slot mRunTime the total run time (hours) of the model
+#' @slot mInitialDensity the density the cells were seeded at
+#' @slot mInheritGrowth whether or not cells inherit growth rates from their parent
+#' @slot mOutputIncrement the frequency of print statements during the run
+#' @slot mRandSeed the random seed 
+#' @slot mEpsilon model specific parameter 
+#' @slot mNG model specific parameter
+#' @slot mTimeIncrement amount of time elapsed in each model step
+#' @slot mCycleLengthDist initial distribution of cell-cycle lengths 
 #' @export
 
 setClass("CellModel", representation(
@@ -31,177 +30,227 @@ setClass("CellModel", representation(
                         mCycleLengthDist = "numeric",
                         mRecordIncrement = "numeric" ))
 
-## getters (parameters)
+#### getters (parameters) ####
 
-.initialNumCells <- function(model) {
+.initialNumCells <- function(model) { return (model@mInitialNumCells) }
 
-    return (model@mInitialNumCells)
+.runTime <- function(model) { return (model@mRunTime) }
 
-}
+.initialDensity <- function(model) { return (model@mInitialDensity) }
 
-.runTime <- function(model) {
+.inheritGrowth <- function(model) { return (model@mInheritGrowth) }
 
-    return (model@mRunTime)
+.outputIncrement <- function(model) { return (model@mOutputIncrement) }
 
-}
+.randSeed <- function(model) { return (model@mRandSeed) }
 
-.initialDensity <- function(model) {
+.epsilon <- function(model) { return (model@mEpsilon) }
 
-    return (model@mInitialDensity)
+.nG <- function(model) { return (model@mNG) }
 
-}
+.timeIncrement <- function(model) { return (model@mTimeIncrement) }
 
-.inheritGrowth <- function(model) {
+.cycleLengthDist <- function(model) { return (model@mCycleLengthDist) }
 
-    return (model@mInheritGrowth)
-
-}
-
-.outputIncrement <- function(model) {
-
-    return (model@mOutputIncrement)
+.recordIncrement <- function(model) { return (model@mRecordIncrement) }
 
 }
 
-.randSeed <- function(model) {
+#### getters (cell data) ####
 
-    return (model@mRandSeed)
-
-}
-
-.epsilon <- function(model) {
-
-    return (model@mEpsilon)
-
-}
-
-.nG <- function(model) {
-
-    return (model@mNG)
-
-}
-
-.timeIncrement <- function(model) {
-
-    return (model@mTimeIncrement)
-
-}
-
-.recordIncrement <- function(model) {
-
-    return (model@mRecordIncrement)
-
-}
-
-.cycleLengthDist <- function(model) {
-
-    return (model@mCycleLengthDist)
-
-}
-
-## getters (cell data)
+#' \code{getCoordinates} get a two dimensional matrix of all the cell coordinates
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return an N X 2 matrix of cell coordinates at time
 
 getCoordinates <- function(model, time) {
 
-    indices <- seq(1,length(model@mCells[[timeToRow(model,time)]]),6)
+    # find the row corresponding to the given time 
+    row <- timeToRow(model, time)
+
+    # get the sequence of indices that contain x-coordinates
+    indices <- seq(1,length(model@mCells[[row]]),6)
+    
+    # create return matrix, col 1 = x-coord & col 2 = y-coord
     ret_mat <- matrix(nrow = length(indices), ncol = 2)
+
+    # for each cell (each index), store the x and y coordinate
     for (i in 1:length(indices)) {
 
-        ret_mat[i,1] = model@mCells[[timeToRow(model,time)]][indices[i]]
-        ret_mat[i,2] = model@mCells[[timeToRow(model,time)]][indices[i]+1]
+        # store x-coordinate
+        ret_mat[i,1] = model@mCells[[row]][indices[i]]
 
-	}
+        # store y-coordinate (x-coord index + 1)
+        ret_mat[i,2] = model@mCells[[row]][indices[i]+1]
 
+    }
+
+    # return the coordinate matrix
     return (ret_mat)
          
 }
 
-getRadii <-	function(model, time) {
+#' \code{getRadii} get the radius of each cell
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return vector containing the radius of each cell at time
 
-    indices <- seq(3,length(model@mCells[[timeToRow(model,time)]]),6)
-    return(model@mCells[[timeToRow(model,time)]][indices])
+getRadii <- function(model, time) {
+
+    # find the row corresponding to the given time
+    row <- timeToRow(model, time)
+
+    # get the sequence of indices that contain the cell radius (starts at 3)
+    indices <- seq(3,length(model@mCells[[row]]),6)
+
+    # return the values at these indices
+    return(model@mCells[[row]][indices])
 
 }
+
+#' \code{getAxisLength} get the axis length of each cell
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return vector containing the axis length of each cell at time
          
 getAxisLength <- function(model, time) {
 
-    indices <- seq(4,length(model@mCells[[timeToRow(model,time)]]),6)
-    return(model@mCells[[timeToRow(model,time)]][indices])
+    # find the row corresponding to the given time
+    row <- timeToRow(model, time)
+
+    # get the sequence of indices that contain the axis length (starts at 4)
+    indices <- seq(4,length(model@mCells[[row]]),6)
+
+    # return the values at these indices
+    return(model@mCells[[row]][indices])
 
 }
+
+#' \code{getAxisAngle} get the axis angle of each cell
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return vector containing the axis angle of each cell at time
 
 getAxisAngle <- function(model, time) {
 
-    indices <- seq(5,length(model@mCells[[timeToRow(model,time)]]),6)
-    return(model@mCells[[timeToRow(model,time)]][indices])
+    # find the row corresponding to the given time
+    row <- timeToRow(model, time)
+
+    # get the sequence of indices that contain the axis angle (starts at 5)
+    indices <- seq(5,length(model@mCells[[row]]),6)
+
+    # return the values at these indices
+    return(model@mCells[[row]][indices])
 
 }
+
+#' \code{getGrowthRates} get the model growth rates of each cell
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return vector containing the growth rate of each cell at time
 
 getGrowthRates <- function(model, time) {
 
-    indices <- seq(6,length(model@mCells[[timeToRow(model,time)]]),6)
-    return(model@mCells[[timeToRow(model,time)]][indices])
+    # find the row corresponding to the given time
+    row <- timeToRow(model, time)
 
-}
+    # get the sequence of indices that contain the cell growth rate (starts at 6)
+    indices <- seq(6,length(model@mCells[[row]]),6)
+
+    # return the values at these indices
+    return(model@mCells[[row]][indices])
+
+
+
+#### exported functions for this class ####
 
 #' \code{getCycleLengths} return the cycle lengths of each cells at time
 #'
 #' @param model a CellModel object
-#' @param time time of interest
+#' @param time time in model hours
 #' @return the cycle lengths of each cell at time
+#' @examples
+#' getCycleLengths(runCancerSim(1,1), 1)
 #' @export
-#'
 
 getCycleLengths <- function(model, time) {
 
+    # get the raw model growth rates
     gr_rates <- getGrowthRates(model, time)
+
+    # convert the raw growth rates to cell cycle time in hours
     return (1 + 2 * (sqrt(2) - 1) * .timeIncrement(model) * .nG(model) / gr_rates)
 
 }
 
-#' \code{show} display summary of CellModel class
+#' \code{getNumberOfCells} get the number of cells alive
 #'
-#' @param object A CellModel Object
-#' @examples show(runCancerSim(1,6))
-#' @rdname CellModel-class
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return the number of cells at this time
+#' @examples 
+#' getNumberOfCells(runCancerSim(1,1), 1)
 #' @export
+
+getNumberOfCells <- function(model, time) {
+
+    return (sum(getRadii(model, time) > 0))   
+
+}
+
+#' \code{getDensity} gets the density of cells at a given time
 #'
+#' @param model A Cell Model
+#' @param time time in model hours
+#' @return The density of cells at that time (not quite the same as confluency)
+#' @examples
+#' getDensity(runCancerSim(1,1),1)
+#' @export
 
-setMethod("show", "CellModel",
+getDensity <- function(model,time) {
 
-    function (object) {
+    # get coordinates of each cell
+    coords <- getCoordinates(model, time)
 
-        cat("model parameters:\n")
-        print(getParameters(object))
-        cat("available functions:\n")
-        cat("interactivePlot\n")
-        cat("getCycleLengths\n")
+    #get radius of each cell
+    radii <- getRadii(model, time)
+    
+    # farthest distance from (0,0) of cell
+    d <- max(sqrt(coords[,1]**2 + coords[,2]**2) + radii)
 
-    }
+    # ratio of area of all cells and the disk which contains them       
+    return(sum(radii ** 2) / (d ^ 2))
 
-)
+}
+
 
 #' \code{getParameters} get a named list of parameters in the model
 #'
 #' @param model A CellModel
-#' @param fullDist whether or not to return full distribution of cycle length
+#' @param fullDist [bool] return full distribution of cycle length
 #' @return a named list of parameters in the model
 #' @examples
 #' getParameters(runCancerSim(1,1))
-#' @rdname CellModel-class
 #' @export
-#'
 
 getParameters <- function(model, fullDist=FALSE) {
 
+    # get the average cell cycle time
     retDist = mean(.cycleLengthDist(model))
 
+    # if the full distribution is requested, store the entire distribution
     if (fullDist) {
 
-     retDist = .cycleLengthDist(model)
+        retDist = .cycleLengthDist(model)
 
     }
 
+    # create a named list of all the parameters
     ret_val = list(
 
         initialNum = .initialNumCells(model),
@@ -217,7 +266,216 @@ getParameters <- function(model, fullDist=FALSE) {
 
     )           
 
+    # return named list
     return(ret_val)
+
+}
+
+#' \code{plotCell} plots a CellModel at a given time
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return plot a visual representation of cells 
+#' @examples plotCells(runCancerSim(10,1), 1)
+#' @export
+
+plotCells <- function(model,time)  {
+
+    # get all the cell information
+    coords <- getCoordinates(model, time)
+    radii <- getRadii(model, time)
+    axis_len <- getAxisLength(model, time)
+    axis_ang <- getAxisAngle(model, time)
+
+    # find a square that contains all cells
+    mn <- min(coords) - 2
+    mx <- max(coords) + 2
+    
+    # create the plot template
+    plot(c(mn,mx),c(mn,mx),main=paste("Plot of CellModel At Time",time),xlab = "",ylab="",type="n",asp=1)
+          
+    # get all (x,y) pairs for each of the two circles that make up a cell
+    x_1 <- coords[,1] + (0.5 * axis_len - radii) * cos(axis_ang)
+    x_2 <- coords[,1] - (0.5 * axis_len - radii) * cos(axis_ang)
+    y_1 <- coords[,2] + (0.5 * axis_len - radii) * sin(axis_ang)
+    y_2 <- coords[,2] - (0.5 * axis_len - radii) * sin(axis_ang)
+
+    # combine all coordinate pairs along with the radii for each cell    
+    x <- c(x_1,x_2)
+    y <- c(y_1,y_2)
+    rad <- c(radii, radii)
+    
+    # plot the cells
+    symbols(x,y, circles=rad, inches=FALSE, add=TRUE, bg="bisque4", fg="bisque4")
+
+}
+
+#' \code{show} display summary of CellModel class
+#'
+#' @param object A CellModel Object
+#' @examples show(runCancerSim(1,1))
+#' @return shows all available functions and parameters of model
+#' @export
+
+setMethod("show", "CellModel",
+
+    function (object) {
+
+        cat("model parameters:")
+        print(getParameters(object))
+        cat("available functions:\n")
+        cat("interactivePlot\n")
+        cat("plotCells\n")
+        cat("getParameters\n")
+        cat("getDensity\n")
+        cat("getCycleLengths\n")
+        cat("getNumberOfCells\n")
+
+    }
+
+)
+
+#' \code{interactivePlot} plots a CellModel and allows the user to control the plot with various commands
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return plot a visual representation of cells that takes in command-line-like inputs, type 'h' for help and a list of all available commands
+#' @export
+
+interactivePlot <- function(model, time = 0) {
+
+    # default arg value    
+    default_arg = 1
+
+    # while time is valid
+    while (time <= .runTime(model) && time >= 0) {
+      
+        # call internal function which plots cells at current time
+        plotCells(model,time)
+
+        # get keyboard input
+        read = readline()
+
+        # find separator between command and arg
+        place = unlist(gregexpr(" ",read))[1]
+
+        if (place == -1) {
+
+            place = nchar(read)
+
+        }
+
+        # parse command
+        cmd = gsub(" ","",substring(read,1,place))
+        arg = suppressWarnings(as.numeric(gsub(" ","",substring(read,place,nchar(read)))))
+        
+        # list of possible commands
+        cmds <- c("n","b","t","i","s","q","h")
+
+        # set to default if no command is present
+        if (is.na(arg)) {
+
+            arg = default_arg
+
+        }
+
+        # check if valid command
+        if ((cmd %in% cmds)) {
+    
+            # switch on which command was provided
+            switch (match(cmd,cmds),
+        
+                # 'n' increase time by arg
+                {time = time + arg},
+
+                # 'b' decrease time by arg
+                {time = time - arg},
+
+                # 't' go to time arg
+                {time = arg},
+
+                # 'i' set default arg
+                {default_arg = arg},
+
+                # 's' get cell summary
+                {
+                    cat("Cell Density = ", getDensity(model,time), "\n")
+                    cat("Number of Cells = ", getNumberOfCells(model, time), "\n")
+                },
+
+                # 'q' quit display
+                {
+                    graphics.off()
+                    break
+                },
+
+                # display help command
+                {
+                    cat("Basic Commands:\n")
+                    cat("b ARG = back ARG timesteps (default ARG = 1)\n")
+                    cat("n ARG = forward ARG timesteps (default ARG = 1)\n")
+                    cat("t ARG - jump to timestep ARG (default ARG = 1)\n")
+                    cat("i ARG - change default ARG for other commands\n")
+                    cat("s = summary of cells\nq = quit\nh = basic command help\n")
+                }
+
+            )
+
+        # invalid command                
+        } else {
+
+            cat("Enter a valid command. Type \"h\" for further help\n")
+
+        }
+
+    }
+
+}
+
+#### helper (non-exported) functions ####
+
+#' \code{getNumNeighbors} return the number of neighboring cells
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @param index index of cell
+#' @return the number of neighbors around the cell at index
+
+getNumNeighbors <- function(model, time, index) {
+
+    # initialize number of neighbors
+    num <- 0 
+
+    # get coordinates of all cells
+    coords <- getCoordinates(model, time)
+    
+    # search through all cells besides the one at index
+    for (i in setdiff(1:nrow(coords), index)) {
+
+        # if radius is within distance of inner ring of neighbors
+        if ((coords[i,1] - coords[index,1])^2 + (coords[i,2] - coords[index,2])^2 < 12) {
+
+            # add to neighbor count
+            num <- num + 1
+
+        }
+
+    }
+
+    # return neighbor count
+    return (num)
+
+}
+
+#' \code{timeToRow} return the correct row in the mCells list corresponding to a given time
+#'
+#' @param model A CellModel
+#' @param time time in model hours
+#' @return corresponding row in mCells list
+
+timeToRow <- function(model, time) {
+
+    return (floor(time / .timeIncrement(model)) + 1)
 
 }
 
--- R/ExternalFunctions.R
+++ R/ExternalFunctions.R
@@ -1,79 +1,3 @@
-#' \code{interactivePlot} Plots a CellModel and allows the user to control the plot with various commands
-#'
-#' @param model A CellModel
-#' @param time The timestep at which to plot the matrix. Must be below
-#'      the specified max amount of timesteps
-#' @return Plot a visual representation of cells that 
-#' takes in command-line-like inputs as well
-#' see "plotCellsAtTime and plotInteractive" in vignette for
-#' command prompt inputs.
-#' @export
-#' @rdname CellModel-class
-
-interactivePlot <- function(model, time = 0) {
-
-    defnum = 1
-    while (time <= .runTime(model)) {
-      
-        plotCellsAtTime(model,time)
-
-        read = readline()
-        place = unlist(gregexpr(" ",read))[1]
-
-        if (place == -1) {
-
-	        place = nchar(read)
-
-        }
-
-        cmd = gsub(" ","",substring(read,1,place))
-        arg_num = suppressWarnings(as.numeric(gsub(" ","",substring(read,place,nchar(read)))))
-        
-        cmds <- c("n","b","t","i","s","q","h")
-
-        if (is.na(arg_num)) {
-
-            arg_num = defnum
-
-        }
-
-		if ((cmd %in% cmds)) {
-	
-			switch (match(cmd,cmds),
-        
-                {time = time + arg_num},
-                {time = time - arg_num},
-                {time = arg_num},
-		        {defnum = arg_num},
-                {
-                    cat("Cell Density = ", getDensity(model,time), "\n")
-					cat("Number of Cells = ", length(getCycleLengthDistribution(model,time)), "\n")
-                },
-                {
-                    graphics.off()
-                    break
-                },
-                {
-                    cat("Basic Commands:\n")
-                    cat("b ARG = back ARG timesteps (default ARG = 1)\n")
-                    cat("n ARG = forward ARG timesteps (default ARG = 1)\n")
-                    cat("t ARG - jump to timestep ARG (default ARG = 1)\n")
-                    cat("i ARG - change default ARG for other commands\n")
-                    cat("s = summary of cells\nq = quit\nh = basic command help\n")
-                }
-
-			)
-
-        } else {
-
-            cat("Enter a valid command. Type \"h\" for further help\n")
-
-        }
-
-    }
-
-}
-
 #'\code{getDrugEffect} 
 #'
 #' @param cycleLengthSeq A sequence spanning the range of cycle lengths
@@ -84,31 +8,41 @@ interactivePlot <- function(model, time = 0) {
 
 getDrugEffect <- function(FUN = function(x) {0}, ...) {
 
+    # store either the cycle lengths themselves or a sequence spanning their range
     cycleLengthDist <- list(...)$cycleLengthDist
     cycleLengthSeq <- list(...)$cycleLengthSeq
 
+    # check which parameters are provided
     if (is.null(cycleLengthDist)) {
 
         if (is.null(cycleLengthSeq)) {
 
+            # need at least one
             stop("must specifiy either cycleLengthDist or cycleLengthSeq")
 
         }
 
+    # if only cycle lengths are provided ...
     } else if (is.null(cycleLengthSeq)) {
 
+        # create a sequence that spans their range
         cycleLengthSeq <- seq(min(cycleLengthDist), max(cycleLengthDist), 0.1)
 
     } 
 
+    # create return list    
     ret_list <- vector("list", length(cycleLengthSeq))
 
+    # for each element in the sequence
     for (i in 1:length(ret_list)) {
 
+        # apply the function to the growth rate, note that FUN could return
+        # vector of values which the model then uses to sample from
         ret_list[[i]] = c(cycleLengthSeq[i], FUN(cycleLengthSeq[i]))
 
     }
 
+    # return the list
     return (ret_list)
 
 }
