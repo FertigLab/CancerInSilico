b7c478c5d9a3e706234b9ae8877985fa06ea653d
Tom Sherman <tsherma4@jhu.edu>
2016-10-10 22:24:50 -0400

    fixed bug

--- src/CellModel.cpp
+++ src/CellModel.cpp
@@ -53,7 +53,7 @@ Rcpp::List CellModel(
     Rcpp::Function setSeed = baseEnv["set.seed"];
     setSeed(randSeed);
 
-    Parameters* params = new Parameters(pow(2,0.5));
+    Parameters* params = new Parameters(pow(2, 0.5));
 
     params->SetMaxTranslation(maxTranslation);
     params->SetMaxDeform(maxDeform);
