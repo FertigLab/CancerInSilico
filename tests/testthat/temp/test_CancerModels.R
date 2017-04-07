6d9f1fc633e1d25a1c000fc3cf34fce64a82e59e
Tom Sherman <tsherma4@jhu.edu>
2017-04-07 14:45:19 -0400

    removed duplicate tests

--- tests/testthat/temp/test_CancerModels.R
+++
@@ -1,8 +0,0 @@
-context("Testing functions in CancerModels.R")
-
-test_that("no NA values in output", {
-  
-	output <- runCancerSim(5,3,0.1, nG = 4)
-	expect_equal(sum(is.na(output@cells)), 0)
-
-})
