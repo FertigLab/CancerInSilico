678f1fb168c1ab141d07f8351e19e8696dce0d1d
rcao5 <rcao5@jhu.edu>
2017-05-17 12:34:37 -0400

    more fixes to print statement

--- inst/scripts/runCancerInSilico.R
+++ inst/scripts/runCancerInSilico.R
@@ -49,7 +49,7 @@ cellTypeInitFreq <- c(initFreq_typeA, 1 - initFreq_typeA)
 # Sanity check for distribution and cycle lengths
 
 print(paste("Cell Type dist:", "A:", cellTypeInitFreq[1], "B:", cellTypeInitFreq[2]))
-print(paste("Cell Type B info:", show(cellTypes[2])))
+print(paste("Cell Type B minCycle:", slot(cellTypes[2][[1]], "minCycle")))
 
 
 #### Run Simulation ####
